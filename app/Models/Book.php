7a216afc6eb390bfd9edd86971c14b078bb90c9a
Mari-Liis <mariliis.sink@gmail.com>
2023-10-12 17:10:05 +0300

    seosed

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Book extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    public function author(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class);
+    }
 
-    public $timestams = false;
+    
+    use HasFactory;
+    public function ordr(): HasOne
+    {
+    return $this->hasOne(orders::class);
+    }
+    
+
  }
--- app/Models/orders.php
+++ app/Models/orders.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasManyMany;
 
 class orders extends Model
 {
     use HasFactory;
+
+    public function book(): HasMany
+    {
+        return $this->hasMany(Book::class);
+    }
+
 }
+
+
